0d3d37562f152f55d96587195d1ae9f38776c377
Hirse <jan.pilzer@gmx.de>
2014-01-16 10:43:14 +0100

    Enable Roman numbering

--- texba.bat
+++ texba.bat
@@ -18,6 +18,7 @@ IF EXIST document.glo (
 biber -q document
 pdflatex -quiet document
 pdflatex -quiet document
+pdflatex -quiet document
 MOVE /y document.pdf ../"%pdfname%" > NUL
 MOVE /y document.log ../texba.log > NUL
 FOR %%i IN (document.*) DO IF NOT %%i == document.tex DEL %%i
